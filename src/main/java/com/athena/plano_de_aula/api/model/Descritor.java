510b3dcbb5f9eac0853dbf7685219b3aca067ecc
caioorleans <caioorleansdm@hotmail.com>
2022-05-04 14:56:39 -0300

    camada de modelo criada

---
+++ src/main/java/com/athena/plano_de_aula/api/model/Descritor.java
@@ -0,0 +1,26 @@
+package com.athena.plano_de_aula.api.model;
+
+public class Descritor {
+	private String id;
+	private String descricao;
+	private Disciplina disciplina;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+}
---
+++ src/main/java/com/athena/plano_de_aula/api/model/PlanoDeAula.java
@@ -0,0 +1,57 @@
+package com.athena.plano_de_aula.api.model;
+
+import java.util.ArrayList;
+
+public class PlanoDeAula {
+	private Integer id;
+	private Integer idRecurso;
+	private String titulo;
+	private String conteudo;
+	private String autor;
+	private Disciplina disciplina;
+	private ArrayList<Descritor> descritores;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getIdRecurso() {
+		return idRecurso;
+	}
+	public void setIdRecurso(Integer idRecurso) {
+		this.idRecurso = idRecurso;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	public ArrayList<Descritor> getDescritores() {
+		return descritores;
+	}
+	public void setDescritores(ArrayList<Descritor> descritores) {
+		this.descritores = descritores;
+	}
+	
+}
