bd3807dfd10707ea7fb34f6fbac6f329daf1f0d0
caioorleans <caioorleansdm@hotmail.com>
2022-05-04 14:43:47 -0300

    projeto criado

---
+++ src/main/java/com/athena/plano_de_aula/api/Application.java
@@ -0,0 +1,13 @@
+package com.athena.plano_de_aula.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
